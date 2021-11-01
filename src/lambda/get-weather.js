98da0c876de8f80936bca47d45e3768d7e9c9058
Pippin <penelopeburkett@gmail.com>
2021-11-01 20:26:21 +0000

    weather API fix

--- src/lambda/get-weather.js
+++ src/lambda/get-weather.js
@@ -4,7 +4,7 @@ export async function handler(event, context) {
 const API_KEY = e314bfeaf3b01a50f44e349007c58dc5
 
 try {
-  const response = await axios.get(`api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`, { headers: { Accept: "application/json" } })
+  const response = await axios.get(`api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`, { headers: { Accept: "application/json" } })
   const data = response.data
   return {
     statusCode: 200,
